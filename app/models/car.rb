2d1c6d16090fec4301f744ec817f5ebd02810f67
Mohammad Mansoor <mansoor6999@gmail.com>
2019-10-04 20:08:19 -0600

    Added validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :make
   has_and_belongs_to_many :parts
+
+  validates :model, presence: true, length: { minimum: 2 }
+  validates :vin, presence: true, length: { minimum: 11 }, numericality: true
+  validates :parts, acceptance: true
 end
--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,6 @@
 class Make < ApplicationRecord
   has_many :cars
+
+  validates :make, presence: true, length: { minimum: 3}
+  validates :country, presence: true, length: { minimum: 4}
 end
